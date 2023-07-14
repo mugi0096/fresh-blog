dc0ad721cb024377e31424986d0c7c26112f5f4f
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-07-14 21:37:22 +0900

    ✨ add Header component

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import { JSX } from "preact";
+
+export default function Header() {
+  return (
+    <header>
+      <h1 class="title px-2 py-1 text-xl">TKS Blog</h1>
+    </header>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,8 @@
 import { Head } from "$fresh/runtime.ts";
 import { useSignal } from "@preact/signals";
+
 import Counter from "../islands/Counter.tsx";
+import Header from "../components/Header.tsx";
 
 export default function Home() {
   const count = useSignal(3);
@@ -10,6 +12,7 @@ export default function Home() {
         <title>Fresh App</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
+        <Header />
         <img
           src="/logo.svg"
           class="w-32 h-32"
